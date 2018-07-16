68e3acdb7a9cb10a7af133849cf4edc50168e58f
byyun <hoi5088@hanmail.net>
2018-07-16 17:41:31 +0900

    git webserver 주석

--- server_side_javascript/webserver.js
+++ server_side_javascript/webserver.js
@@ -6,6 +6,6 @@ const port = 1337;
 http.createServer((req, res) => {
  res.writeHead(200, { 'Content-Type': 'text/plain' });
  res.end('Hello World\n');
-}).listen(port, hostname, () => {
+}).listen(port, hostname, () => { //서버를 만들고 해당 포트, 호스트네임에 리스닝하도록 처리
  console.log(`Server running at http://${hostname}:${port}/`);
 });
